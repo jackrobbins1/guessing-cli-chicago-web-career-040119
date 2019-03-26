5e9784b132bfbe49e24425028958e041d255e4ce
Jack Robbins <jackrobbins1@gmail.com>
2019-03-26 15:20:07 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,6 @@
 # Code your solution here!
+# Greet user and tell them to type in a number between 1 to 10
+def greetUsr
+  puts "Welcome to Guessing Game!
+  Please enter a number between 1-10"
+end
